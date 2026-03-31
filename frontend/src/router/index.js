761f8a6c5cb866e807d48da2ea29be47ecabe81e
guswowls2017 <72922734+guswowls2017@users.noreply.github.com>
2026-03-31 10:15:24 +0900

    Add CardPage and OrderManagementPage routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+
+import CardPage from "../views/CardPage.vue"
+import OrderManagementPage from "../views/OrderManagementPage.vue"
+
+const routes = [
+    {
+        path: "/cards",
+        component: CardPage
+    },
+    {
+        path: "/orders",
+        component: OrderManagementPage,
+        meta: { title: "주문 관리" }
+    }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+    history: createWebHistory(),
+    routes
 })
 
 export default router
