a41a547093a51c97200207ae4a8a2dd22711b432
guswowls2017 <72922734+guswowls2017@users.noreply.github.com>
2026-03-31 10:16:31 +0900

    Add files via upload

---
+++ frontend/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <router-view/>
+
+</template>
+
+<script>
+
+import CardPage from "./views/CardPage.vue"
+import OrderManagementPage from "./views/OrderManagementPage.vue"
+
+export default{
+
+  components:{
+    CardPage,
+    OrderManagementPage
+  }
+
+}
+
+</script>
