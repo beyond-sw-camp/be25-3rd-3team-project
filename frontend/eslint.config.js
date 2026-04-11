c4ae2ef4f442d7d30a46cc178706323e5f7c8455
LeeJin0801 <leetrue801@gmail.com>
2026-04-11 17:44:35 +0900

    프론트엔드 공통 설정 및 아이콘 라이브러리 추가

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import prettier from '@vue/eslint-config-prettier'
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        clearInterval: 'readonly',
+        clearTimeout: 'readonly',
+        console: 'readonly',
+        document: 'readonly',
+        fetch: 'readonly',
+        localStorage: 'readonly',
+        navigator: 'readonly',
+        process: 'readonly',
+        setInterval: 'readonly',
+        setTimeout: 'readonly',
+        window: 'readonly',
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+]
